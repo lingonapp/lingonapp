53292be7056f6c261265d543248bc6efe82d470a
mackattack3k <marcushanssonmh@gmail.com>
2019-09-24 01:06:09 +0200

    Formatting and typing

--- lib/register/bloc/register_bloc.dart
+++ lib/register/bloc/register_bloc.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:lingon/auth/userrepository.dart';
 import 'package:lingon/register/bloc/register_event.dart';
@@ -8,34 +9,32 @@ import 'package:rxdart/rxdart.dart';
 import 'package:validate/validate.dart';
 
 class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
-  final UserRepository _userRepository;
-
   RegisterBloc({@required UserRepository userRepository})
       : assert(userRepository != null),
         _userRepository = userRepository;
+  final UserRepository _userRepository;
 
   @override
   RegisterState get initialState => RegisterState.empty();
 
   @override
-  Stream<RegisterState> transformEvents(
-      Stream<RegisterEvent> events,
-      Stream<RegisterState> Function(RegisterEvent event) next,
-      ) {
-    final observableStream = events as Observable<RegisterEvent>;
-    final nonDebounceStream = observableStream.where((event) {
-      return (event is! EmailChanged && event is! PasswordChanged);
+  Stream<RegisterState> transformEvents(Stream<RegisterEvent> events,
+      Stream<RegisterState> Function(RegisterEvent event) next,) {
+    final Observable<RegisterEvent> observableStream = events;
+    final Observable<RegisterEvent> nonDebounceStream =
+    observableStream.where((RegisterEvent event) {
+      return event is! EmailChanged && event is! PasswordChanged;
     });
-    final debounceStream = observableStream.where((event) {
-      return (event is EmailChanged || event is PasswordChanged);
-    }).debounceTime(Duration(milliseconds: 300));
-    return super.transformEvents(nonDebounceStream.mergeWith([debounceStream]), next);
+    final Observable<RegisterEvent> debounceStream =
+    observableStream.where((RegisterEvent event) {
+      return event is EmailChanged || event is PasswordChanged;
+    }).debounceTime(const Duration(milliseconds: 300));
+    return super.transformEvents(
+        nonDebounceStream.mergeWith([debounceStream]), next);
   }
 
   @override
-  Stream<RegisterState> mapEventToState(
-      RegisterEvent event,
-      ) async* {
+  Stream<RegisterState> mapEventToState(RegisterEvent event,) async* {
     if (event is EmailChanged) {
       yield* _mapEmailChangedToState(event.email);
     } else if (event is PasswordChanged) {
@@ -64,10 +63,8 @@ class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
     );
   }
 
-  Stream<RegisterState> _mapFormSubmittedToState(
-      String email,
-      String password,
-      ) async* {
+  Stream<RegisterState> _mapFormSubmittedToState(String email,
+      String password,) async* {
     yield RegisterState.loading();
     try {
       await _userRepository.signUp(
