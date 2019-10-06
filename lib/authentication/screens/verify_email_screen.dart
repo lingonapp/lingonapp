1e383972d4f03e14e2543e5a8c063ae009053793
Chris015 <christopher.olsson@hyperisland.se>
2019-10-06 15:13:55 +0200

    Add resend email button

--- lib/authentication/screens/verify_email_screen.dart
+++ lib/authentication/screens/verify_email_screen.dart
@@ -1,13 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lingon/theme.dart';
 
 class VerifyEmail extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> _resendEmail() async {
+    final FirebaseUser user = await _auth.currentUser();
+    if (user != null) {
+      user.sendEmailVerification();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Please verify email address')
-      )
-    );
+    return Scaffold(
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+          const Text('Please verify your email address'),
+          FlatButton(
+            color: lingonTheme.primaryColor,
+            textColor: Colors.white,
+            onPressed: () {
+              _resendEmail();
+            },
+            child: const Text(
+              'Resend email',
+            ),
+          )
+        ])));
   }
 }
